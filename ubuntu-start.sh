1db6439a82dc30eaf4515d81f72dac9090be9882
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-20 21:01:34 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -7,7 +7,7 @@
 #touch target/linux/*/Makefile
 
 sudo apt-get update
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+sudo apt-get -y install gcc binutils bzip2 flex python perl make find grep diff, unzip gawk getopt subversion libz-dev and libc headers installed
 git clone -bopenwrt-18.06 https://github.com/sw586/openwrt
 cd openwrt
 ./scripts/feeds update -a
